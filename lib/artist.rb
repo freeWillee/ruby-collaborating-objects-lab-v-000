4e605566fba076b6e5d910d02f0370f918614e6c
Will Lee <will.i.lee@outlook.com>
2018-07-06 17:17:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,10 +27,10 @@ class Artist
 
   def self.find_or_create_by_name(name)
     artist = @@all.detect do |artist|
-      # binding.pry
+      
       artist.name == name
     end
-
+binding.pry
     if artist.nil?
       artist = Artist.new(name)
       artist.save

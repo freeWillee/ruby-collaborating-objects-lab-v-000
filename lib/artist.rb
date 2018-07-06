cde3e22fb5619e6ac7eb156ef4c66e9acfee1c53
Will Lee <will.i.lee@outlook.com>
2018-07-06 16:54:36 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,47 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(name)
+    @songs << name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def songs
+    @songs
+  end
+
+
+  def self.find_or_create_by_name(name)
+    artist = @@all.detect do |artist|
+      binding.pry
+      artist.name = name
+    end
+
+    if artist.nil?
+      artist = Artist.new(name)
+      artist.save
+      # binding.pry
+    end
+    artist
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+end
